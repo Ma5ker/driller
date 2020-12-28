85ce225cbb7a2e5ce0ded9f35e5030dca26dc935
Ma5ker <kcahlb@gmail.com>
2020-12-28 11:14:03 +0800

    add comments to driller_main

--- driller/driller_main.py
+++ driller/driller_main.py
@@ -34,8 +34,8 @@ class Driller(object):
         self.binary      = binary
 
         # Redis channel identifier.
-        self.identifier  = os.path.basename(binary)
-        self.input       = input_str
+        self.identifier  = os.path.basename(binary)# 程序完整地址
+        self.input       = input_str #输入字符
         self.fuzz_bitmap = fuzz_bitmap
         self.tag         = tag
         self.redis       = redis
@@ -53,9 +53,11 @@ class Driller(object):
         self.start_time = time.time()
 
         # Set of all the generated inputs.
+        #所有生成的输入集合
         self._generated = set()
 
         # Set the memory limit specified in the config.
+        #限制内存
         if config.MEM_LIMIT is not None:
             resource.setrlimit(resource.RLIMIT_AS, (config.MEM_LIMIT, config.MEM_LIMIT))
 
@@ -67,7 +69,7 @@ class Driller(object):
         """
         Perform the drilling, finding more code coverage based off our existing input base.
         """
-
+        #redis部分都是分布式的东西 先不管
         # Don't re-trace the same input.
         if self.redis and self.redis.sismember(self.identifier + '-traced', self.input):
             return -1
@@ -82,7 +84,7 @@ class Driller(object):
         if self.redis:
             self.redis.sadd(self.identifier + '-traced', self.input)
 
-        list(self._drill_input())
+        list(self._drill_input()) #  启动driller 获取新生成的输出
 
         if self.redis:
             return len(self._generated)
@@ -108,8 +110,10 @@ class Driller(object):
         """
 
         # initialize the tracer
+        #构建tracer 和angr工程
         r = tracer.qemu_runner.QEMURunner(self.binary, self.input, argv=self.argv)
         p = angr.Project(self.binary)
+        #处理hook
         for addr, proc in self._hooks.items():
             p.hook(addr, proc)
             l.debug("Hooking %#x -> %s...", addr, proc.display_name)
@@ -120,11 +124,14 @@ class Driller(object):
             s = p.factory.entry_state(stdin=angr.SimFileStream, flag_page=r.magic, mode='tracing')
         else:
             s = p.factory.full_init_state(stdin=angr.SimFileStream, mode='tracing')
-
+        #预约束
         s.preconstrainer.preconstrain_file(self.input, s.posix.stdin, True)
 
         simgr = p.factory.simulation_manager(s, save_unsat=True, hierarchy=False, save_unconstrained=r.crash_mode)
-
+        #trace Oppologist和DrillerCore三种探索技术
+        # trace ：按给定trace进行符号执行
+        # Oppologist：遇到不支持的指令 则全部借用unicorn 进行concrete execution
+        # DrillerCore： driller模块，将感兴趣的状态结果存放在diverted stash
         t = angr.exploration_techniques.Tracer(trace=r.trace, crash_addr=r.crash_addr, copy_states=True)
         self._core = angr.exploration_techniques.DrillerCore(trace=r.trace, fuzz_bitmap=self.fuzz_bitmap)
 
@@ -147,12 +154,13 @@ class Driller(object):
             if 'diverted' not in simgr.stashes:
                 continue
 
-            while simgr.diverted:
-                state = simgr.diverted.pop(0)
+            while simgr.diverted:#对于diverted stash的每个状态
+                state = simgr.diverted.pop(0)# 每次pop 一个找到的状态
                 l.debug("Found a diverted state, exploring to some extent.")
-                w = self._writeout(state.history.bbl_addrs[-1], state)
+                w = self._writeout(state.history.bbl_addrs[-1], state) #对每个状态转换，得到相应的输入
                 if w is not None:
                     yield w
+                #符号执行探索部分
                 for i in self._symbolic_explorer_stub(state):
                     yield i
 
@@ -173,18 +181,20 @@ class Driller(object):
         simgr = p.factory.simulation_manager(state, hierarchy=False)
 
         l.debug("[%s] started symbolic exploration at %s.", self.identifier, time.ctime())
-
+        #探索约束 
         while len(simgr.active) and accumulated < 1024:
             simgr.step()
             steps += 1
 
             # Dump all inputs.
-            accumulated = steps * (len(simgr.active) + len(simgr.deadended))
+            accumulated = steps * (len(simgr.active) + len(simgr.deadended))#？？  步数乘以状态数来设置符号执行器探索的上限约束
 
         l.debug("[%s] stopped symbolic exploration at %s.", self.identifier, time.ctime())
 
         # DO NOT think this is the same as using only the deadended stashes. this merges deadended and active
+        #把deadend桶的状态全移到active中一起处理
         simgr.stash(from_stash='deadended', to_stash='active')
+        #导出所有状态的输入
         for dumpable in simgr.active:
             try:
                 if dumpable.satisfiable():
@@ -239,22 +249,26 @@ class Driller(object):
         # No redis = no catalogue.
 
     def _writeout(self, prev_addr, state):
+        #求解此状态输出
         generated = state.posix.stdin.load(0, state.posix.stdin.pos)
         generated = state.solver.eval(generated, cast_to=bytes)
-
+        #输出长度,前一基本块的地址,此状态地址 
         key = (len(generated), prev_addr, state.addr)
 
         # Checks here to see if the generation is worth writing to disk.
         # If we generate too many inputs which are not really different we'll seriously slow down AFL.
+        #检查独特性？ no redis, return false
         if self._in_catalogue(*key):
             self._core.encounters.remove((prev_addr, state.addr))
             return None
 
         else:
+            # no redis, do nothing 
             self._add_to_catalogue(*key)
 
         l.debug("[%s] dumping input for %#x -> %#x.", self.identifier, prev_addr, state.addr)
 
+        #输入加入 self._generated
         self._generated.add((key, generated))
 
         if self.redis:
